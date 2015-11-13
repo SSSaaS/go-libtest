f1638070d38e53a0161e7321a78888050802ea8c
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-11-12 21:57:32 -0600

    Utility functionality

--- go-libtest.go
+++ go-libtest.go
@@ -11,31 +11,46 @@ import (
 )
 
 func main() {
-	settings_file, err := ioutil.ReadFile("libtest.json")
-	if err != nil {
-		panic(err)
+	if (len(os.Args) == 1) {
+		fmt.Println("Error! Must specify at least one spec file:")
+		fmt.Println("")
+		fmt.Println("Usage: go-libtest spec1.json [spec2.json ... specn.json]")
 	}
 
-	os.RemoveAll("./tmp")
-	os.Mkdir("./tmp", 0777)
-
-	var settings_struct settings.Settings
-	err = json.Unmarshal(settings_file, &settings_struct)
-	if err != nil {
-		panic(err)
-	}
-
-	t1 := time.Now()
-	for t := range settings_struct.Tests {
-		test := settings_struct.Tests[t]
-		tester := tests.Tests{test.Method, settings_struct}
-		if test.Type == "XChain" {
-			tester.XChain(test.Data, test.Input, test.Output, test.Result)
+	for i := range os.Args[1:] {
+		if _, err := os.Stat(os.Args[i+1]); os.IsNotExist(err) {
+			fmt.Printf("no such file or directory: %s", os.Args[i+1])
+			return
 		}
-	}
 
-	duration := time.Since(t1)
-	fmt.Println("------")
-	fmt.Println("total:", fmt.Sprintf("%.3f", duration.Seconds()), "s")
-	os.RemoveAll("./tmp")
+		fmt.Println("loading specs: " + os.Args[i+1])
+
+		settings_file, err := ioutil.ReadFile(os.Args[i+1])
+		if err != nil {
+			panic(err)
+		}
+
+		os.RemoveAll("./tmp")
+		os.Mkdir("./tmp", 0777)
+
+		var settings_struct settings.Settings
+		err = json.Unmarshal(settings_file, &settings_struct)
+		if err != nil {
+			panic(err)
+		}
+
+		t1 := time.Now()
+		for t := range settings_struct.Tests {
+			test := settings_struct.Tests[t]
+			tester := tests.Tests{test.Method, settings_struct}
+			if test.Type == "XChain" {
+				tester.XChain(test.Data, test.Input, test.Output, test.Result)
+			}
+		}
+
+		duration := time.Since(t1)
+		fmt.Println("------")
+		fmt.Println("total:", fmt.Sprintf("%.3f", duration.Seconds()), "s\n")
+		os.RemoveAll("./tmp")
+	}
 }
